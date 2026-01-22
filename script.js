f75c210dad54b70e93d2128f81c4607436ffae92
André Balbi <amabalbi@id.uff.br>
2026-01-22 19:10:08 -0300

    fix: Adds init version

---
+++ script.js
@@ -0,0 +1,13 @@
+function helloWorld(){
+    console.log("Hello World");
+}
+
+function convert(){
+    let length  =   document.getElementById("length");     
+    let from    =   document.getElementById("from"); 
+    let to      =   document.getElementById("to"); 
+    
+    console.log(length.value);
+    console.log(from.value);
+    console.log(to.value);
+}
