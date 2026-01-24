a723b3c6dc523dc5098e2bdf192ba66e069c2c88
André Balbi <amabalbi@id.uff.br>
2026-01-24 13:46:55 -0300

    fix: Adds basic js capabilities. scratch css

--- script.js
+++ script.js
@@ -1,13 +1,54 @@
-function helloWorld(){
-    console.log("Hello World");
+const convertElement = document.getElementById("convert-button");
+
+function fromMeters(value, toUnit) {
+    const meterRates = {
+        mile: 0.000621371,
+        km: 0.001,
+        feet: 3.28084,
+        centimeter: 100,
+        meter: 1
+    };
+
+    return value * meterRates[toUnit];
+
 }
 
-function convert(){
-    let length  =   document.getElementById("length");     
-    let from    =   document.getElementById("from"); 
-    let to      =   document.getElementById("to"); 
-    
-    console.log(length.value);
-    console.log(from.value);
-    console.log(to.value);
+function fromCentimeters(value, toUnit) {
+    const meterRates = {
+        mile: 0.00000621371,
+        km: 0.00001,
+        feet: 0.0328084,
+        meter: 0.01,
+        centimeter: 1
+    };
+
+    return value * meterRates[toUnit];
+
 }
+
+function centimeterToMeter(centimeter){
+    return fromCentimeters(centimeter, "meter")
+}
+function meterToCentimeter(meter){
+    return fromMeters(meter, "centimeter")
+}
+
+const conversionMap = {
+    "meter-centimeter" : meterToCentimeter,
+    "centimeter-meter" : centimeterToMeter
+}
+
+function convert(){ 
+    const lengthElement = document.getElementById("length");     
+    const toElement = document.getElementById("to"); 
+    const fromElement = document.getElementById("from"); 
+    const answerElement = document.getElementById("answer");
+
+    // get length 
+    fromValue = fromElement.value;
+    toValue = toElement.value;
+    
+    answerElement.innerHTML = conversionMap[fromValue + "-" + toValue](lengthElement.value);
+}
+
+convertElement.addEventListener("click", convert)
