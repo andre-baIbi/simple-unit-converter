08695a16afabe83a8f7b3fcedb73751c10ffc2da
André Balbi <amabalbi@id.uff.br>
2026-02-26 18:21:39 -0300

    feat(weight): Adds weight conversion feature. Logic rewritten (less hardcoding). Adds basic integration between pages
    ]

--- script.js
+++ script.js
@@ -9,38 +9,97 @@ const meterRates = {
     inch: 39.3701
 };
 
+const kilogramRates = {
+    kilogram: 1,
+    gram: 1000,
+    milligram: 1000000,
+    metric_ton: 0.001,
+    pound: 2.20462,
+    ounce: 35.274,
+    stone: 0.157473
+};
+
+function fromUnitUsingRate(value, toUnit, unit){
+    switch (unit){
+        case "length":
+            return value * meterRates[toUnit];
+        case "weight":
+            return value * kilogramRates[toUnit];
+    }
+}
+
+function toUnitUsingRate(value, toUnit, unit){
+    switch (unit){
+        case "length":
+            return value / meterRates[toUnit];
+        case "weight":
+            return value / kilogramRates[toUnit];
+    }
+}
+
 function fromMeters(value, toUnit) {
-    return value * meterRates[toUnit];
+    return fromUnitUsingRate(value, toUnit, "length")
 }
 
 function toMeters(value, toUnit) { 
-    return value / meterRates[toUnit];
+    return toUnitUsingRate(value, toUnit, "length")
 }
 
-function converter(value, fromUnit, toUnit){
+function fromKilograms(value, toUnit) {
+    return fromUnitUsingRate(value, toUnit, "weight")
+}
+
+function toKilograms(value, toUnit) { 
+    return toUnitUsingRate(value, toUnit, "weight")
+}
+
+function length_converter(value, fromUnit, toUnit){
     return(fromMeters(toMeters(value, fromUnit), toUnit))
 }
 
+function weight_converter(value, fromUnit, toUnit){
+    return(fromKilograms(toKilograms(value, fromUnit), toUnit))
+}
 
-function convert(){ 
-    const lengthElement = document.getElementById("length");     
+
+function convert(logic){
+    // 0 -> Length / 1 -> Weight
+
+    const inputElement = document.getElementById("input");     
     const toElement = document.getElementById("to"); 
     const fromElement = document.getElementById("from"); 
     const answerElement = document.getElementById("answer");
     const measurementPElement = document.getElementById("measurementName")
     const answerSection = document.getElementById("answer-section")
-    
 
-    // get length 
+    
     fromValue = fromElement.value;
     toValue = toElement.value;
     answerSection.removeAttribute("hidden")
-    answerElement.innerHTML = converter(lengthElement.value, fromValue, toValue);
+    
+    console.log(logic)
 
-    //answerElement.innerHTML = conversionFunctionsMap[fromValue + "-" + toValue](lengthElement.value);
+    switch (parseInt(logic)){
+        case 0:
+            answerElement.innerHTML = length_converter(inputElement.value, fromValue, toValue);
+            break;
+
+        case 1:
+            answerElement.innerHTML = weight_converter(inputElement.value, fromValue, toValue);
+            break;
+        
+        default:
+            answerElement.innerHTML = "Error : Check JS";
+            break;
+    }
+    
 
     // set measurement name as in options
     measurementPElement.innerHTML = toElement.options[toElement.selectedIndex].text;
 }
 
-convertElement.addEventListener("click", convert)
+convertElement.addEventListener("click", (e) => {
+    e.preventDefault();
+    const mode = e.target.getAttribute("data-mode"); 
+    convert(mode); 
+});
