8126d07a8a3d6105c03e64ecfdbf6015792d3a4f
André Balbi <amabalbi@id.uff.br>
2026-02-26 17:36:29 -0300

    feat(alpha): Adds main menu. Optimizes the logic from unit conversion. Changes the layout a little bit. + minor fixes

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ const convertElement = document.getElementById("convert-button");
 
 const meterRates = {
     mile: 0.000621371,
-    km: 0.001,
+    kilometer: 0.001,
     feet: 3.28084,
     centimeter: 100,
-    meter: 1
+    meter: 1,
+    inch: 39.3701
 };
 
 function fromMeters(value, toUnit) {
@@ -16,40 +17,30 @@ function toMeters(value, toUnit) {
     return value / meterRates[toUnit];
 }
 
-function feetToMeter(feet){
-    return toMeters(feet, "feet")
+function converter(value, fromUnit, toUnit){
+    return(fromMeters(toMeters(value, fromUnit), toUnit))
 }
 
-function meterToFeet(meter){
-    return fromMeters(meter, "feet")
-}
-
-function centimeterToMeter(centimeter){
-    return toMeters(centimeter, "centimeter")
-}
-function meterToCentimeter(meter){
-    return fromMeters(meter, "centimeter")
-}
-
-const conversionMap = {
-    "meter-centimeter" : meterToCentimeter,
-    "centimeter-meter" : centimeterToMeter,
-    "feet-meter" : feetToMeter,
-    "meter-feet" : meterToFeet
-    // todo centimeter to feet
-}
 
 function convert(){ 
     const lengthElement = document.getElementById("length");     
     const toElement = document.getElementById("to"); 
     const fromElement = document.getElementById("from"); 
     const answerElement = document.getElementById("answer");
+    const measurementPElement = document.getElementById("measurementName")
+    const answerSection = document.getElementById("answer-section")
+    
 
     // get length 
     fromValue = fromElement.value;
     toValue = toElement.value;
-    
-    answerElement.innerHTML = conversionMap[fromValue + "-" + toValue](lengthElement.value);
+    answerSection.removeAttribute("hidden")
+    answerElement.innerHTML = converter(lengthElement.value, fromValue, toValue);
+
+    //answerElement.innerHTML = conversionFunctionsMap[fromValue + "-" + toValue](lengthElement.value);
+
+    // set measurement name as in options
+    measurementPElement.innerHTML = toElement.options[toElement.selectedIndex].text;
 }
 
 convertElement.addEventListener("click", convert)
